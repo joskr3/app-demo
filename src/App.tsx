56b7eb3e351e60f80a5796bbe2dd51ae227f6457
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-10 19:45:26 -0500

    agregando rutas al proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Vistas/Home';
+import About from './Vistas/About';
+import Product from './Vistas/Product';
+import Login from './Vistas/Login';
+import Registro from './Vistas/Registro';
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registro />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
