a9778f088749f5e068db5c7c046fdf484940c201
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-10 20:30:04 -0500

    implemtando el header y el footer

--- src/Componentes/Footer/index.tsx
+++ src/Componentes/Footer/index.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+
+import { AppBar, Box, Button, Container, IconButton, Link, Toolbar, Typography } from "@mui/material"
+
+function Copyright() {
+  return (
+    <Container >
+      <Typography
+        sx={{ color: "#E4FEFA" }}
+        variant="body2"
+        color="textSecondary"
+        align="left"
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://app-travel.com/">
+          www.app-travel.com
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </Container>
+  );
+}
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ backgroundColor: "#41927F", justifyContent: "flexStart" }} >
+          <Copyright />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#E4FEFA" }}>
+            Created by  codiGo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
--- src/Componentes/Header/index.tsx
+++ src/Componentes/Header/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ backgroundColor: "#E4FEFA" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon  sx={{ color: "#41927F" }} />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#41927F" }}>
+            App travel
+          </Typography>
+          <Button sx={{ color: "#41927F" }}>Iniciar sesión</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Header
+export default Header;
